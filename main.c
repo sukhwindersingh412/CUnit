572314d8005f20ae20d8a73efa59522ad521b982
sukhwindersingh412 <49254880+sukhwindersingh412@users.noreply.github.com>
2021-03-03 13:56:24 -0500

    Update main.c

--- main.c
+++ main.c
@@ -31,4 +31,24 @@ test_div(void){
   CU_ASSERT(add(-20,4)==-5);
 }
     
-
+int main(){
+ 
+  CU_initialize_registry();
+  
+  CU_pSuite suite1 = CU_add_suite("test_add", 0, 0);
+  CU_pSuite suite2 = CU_add_suite("test_sub", 0, 0);
+  CU_pSuite suite3 = CU_add_suite("test_mul", 0, 0);
+  CU_pSuite suite4 = CU_add_suite("test_div", 0, 0);
+  
+  CU_add_suite(suite1, "test_add", test_add);
+  CU_add_suite(suite2, "test_sub", test_sub);
+  CU_add_suite(suite3, "test_mul", test_mul);
+  CU_add_suite(suite4, "test_div", test_div);
+  
+  CU_basic_set_mode(CU_BRM_VERBOSE);
+  CU_basic_run_tests();
+  CU_cleanup_registry();
+  
+  return 0; 
+  
+}

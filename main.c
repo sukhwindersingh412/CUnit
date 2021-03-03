96b79ff5c6560abba99e4ce243aff3f7cfc5730c
sukhwindersingh412 <49254880+sukhwindersingh412@users.noreply.github.com>
2021-03-03 16:56:11 -0500

    Update main.c

--- main.c
+++ main.c
@@ -3,35 +3,35 @@
 #include <CUnit/Basic.h>
 #include 'functions.h'
 
-test_add(void){
+void test_add(void){
   CU_ASSERT(add(0,0)==0);
   CU_ASSERT(add(0,2)==2);
   CU_ASSERT(add(2,3)==5);
   CU_ASSERT(add(-2,5)==3);
 }
     
-test_sub(void){
+void test_sub(void){
   CU_ASSERT(add(0,0)==0);
   CU_ASSERT(add(0,2)==-2);
   CU_ASSERT(add(3,2)==1);
   CU_ASSERT(add(-2,5)==-8);
 }
 
-test_mul(void){
+void test_mul(void){
   CU_ASSERT(add(0,3)==0);
   CU_ASSERT(add(1,2)==2);
   CU_ASSERT(add(2,3)==6);
   CU_ASSERT(add(-2,5)==-10);
 }
 
-test_div(void){
+void test_div(void){
   CU_ASSERT(add(0,3==0);
   CU_ASSERT(add(4,2)==2);
   CU_ASSERT(add(10,3)=2);
   CU_ASSERT(add(-20,4)==-5);
 }
     
-int main(){
+int main() {
  
   CU_initialize_registry();
   
